6c2abad90deec2e61735f72361f2446faeb1a66b
Oleh Hladkov <oleh_hladkov@epam.com>
2022-10-11 19:10:30 +0200

    Add counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ComponentsList from './components/ComponentsList.js';
+import Counter from './components/Counter.js';
 
 function App() {
   return (
     <div className="App">
       <ComponentsList />
+      <Counter />
     </div>
   );
 }
