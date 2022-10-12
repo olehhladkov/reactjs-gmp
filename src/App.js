e5013b77d50d1182d290d3b4536ec50b0f05b319
Oleh Hladkov <oleh_hladkov@epam.com>
2022-10-12 15:50:59 +0200

    Add SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import ComponentsList from './components/ComponentsList.js';
 import Counter from './components/Counter.js';
+import SearchForm from './components/SearchForm.js';
 
 function App() {
   return (
     <div className="App">
       <ComponentsList />
+      <hr />
       <Counter />
+      <hr />
+      <SearchForm />
     </div>
   );
 }
