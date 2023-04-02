6c7b46cf0df007de6b4f47d33c3c5a56d19e228e
oleh_hladkov <oleh_hladkov@epam.com>
2023-04-02 15:55:18 +0200

    Add search form functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,42 @@
+import { useFormik } from 'formik';
+import { getMoviesThunk } from '../app/appSlice';
+import { useDispatch } from 'react-redux';
+
 import '../styles/SearchForm.scss';
 
 function SearchForm() {
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      search: '',
+    },
+    onSubmit: async () => {
+      await dispatch(
+        getMoviesThunk(`?searchBy=title&search=${formik.values.search}`)
+      );
+    },
+  });
+
   return (
     <div className="search-form">
-      <form action="#">
-        <label className="search-form__label" htmlFor="search-term">
+      <form onSubmit={formik.handleSubmit}>
+        <label className="search-form__label" htmlFor="search">
           Find your movie
         </label>
 
         <div className="search-form__row">
           <input
-            id="search-term"
+            id="search"
             className="search-form__input"
             type="search"
             placeholder="What do you want to watch?"
+            onChange={formik.handleChange}
+            value={formik.values.search}
           />
-          <button className="search-form__btn">Search</button>
+          <button type="submit" className="search-form__btn">
+            Search
+          </button>
         </div>
       </form>
     </div>
